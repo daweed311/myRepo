3f7e1a7b6642214c3252452d9607240d1e92bd58
David Melnik <davidmelnik483@gmail.com>
2022-01-30 14:52:02 +0200

    1 урок домашка

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const a = 1;
-let b = "second variable";
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert("Сообщение с любым текстом)");
+
+console.log("Any string");
