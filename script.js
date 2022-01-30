8212a223888ccdaeac5b80f963006b2c457bd124
David Melnik <davidmelnik483@gmail.com>
2022-01-30 15:50:01 +0200

    2 урок домашка

---
+++ script.js
@@ -0,0 +1,22 @@
+const title = "Project";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 33;
+const rollback = 99;
+const fullPrice = 1;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(
+  `“Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани” и “Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани”`
+);
+
+console.log(screens.toLocaleLowerCase().split(" "));
+
+console.log(
+  `Процент откуата посреднику за работу: ${fullPrice * (rollback / 100)}`
+);
